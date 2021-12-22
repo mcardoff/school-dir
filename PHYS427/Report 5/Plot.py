8288af06b3788c42e23c1672e880f5cc7122b0c8
Michael Cardiff <mcardiff0321@gmail.com>
2021-12-21 19:56:21 -0600

    end of semester :)

--- PHYS427/Report 5/Plot.py
+++ PHYS427/Report 5/Plot.py
@@ -1,16 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = np.array([ x / 10 for x in [0.0, 1.0, 2.1, 3.8, 4.7]])
-counts = np.array(
-    [ np.log(x / 72052) for x in [72052, 64517, 56976, 46573, 41182]])
+filename = "SiPlot"
+
+counts = [10459, 9617, 3738]
+ismm = True
+dvals = [0.0, 4.25, 80.65]
+d = np.array([ x / 10 for x in dvals]) if ismm else dvals
+normcounts = np.array([ np.log(x / max(counts)) for x in counts])
 
 fig = plt.figure()
 
+plt.rc('font', size=15)          # controls default text sizes
+plt.rc('axes', titlesize=15)     # fontsize of the axes title
+plt.rc('axes', labelsize=15)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
+plt.rc('legend', fontsize=15)    # legend fontsize
+plt.rc('figure', titlesize=20)   # fontsize of the figure title
+
 ax1 = fig.add_subplot(111)
-ax1.set_ylabel("N")
+ax1.set_ylabel("ln(N) (counts)")
 ax1.set_xlabel("d (cm)")
 ax1.set_title("ln(I/I_0) vs d")
 
-plt.scatter(d, counts)
+plt.scatter(d, normcounts, color='red')
+
+linear_model = np.polyfit(d, normcounts, 1)
+linear_model_fn = np.poly1d(linear_model)
+plt.plot(d,linear_model_fn(d))
+
+plt.text(max(d)-4, max(normcounts)-0.1,
+         'ln(N/N_0)={0[0]:.3g}d+{0[1]:.3g}'.format(linear_model), fontsize = 12)
+
+plt.savefig("./figs/" + filename + ".png")
+
 plt.show()
